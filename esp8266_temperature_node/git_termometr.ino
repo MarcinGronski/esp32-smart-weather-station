f78858c5922c821dd93a3fc495eb88b3ce2b2388
Marcin Groński <marcingronski@gmail.com>
2026-08-17 18:42:59 +0200

    Add files via upload

--- esp8266_temperature_node/git_termometr.ino
+++ esp8266_temperature_node/git_termometr.ino
@@ -1,85 +1,52 @@
-#include <OneWire.h>
-#include <DallasTemperature.h>
-#include <ESP8266WiFi.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <HTTPClient.h>
 
-// Konfiguracja WiFi
 const char* ssid = "";
 const char* password = "";
 
-// Ustawienia statycznego IP
-IPAddress local_IP(192, 168, 0, 105);
-IPAddress gateway(192, 168, 0, 1);       // Dostosuj do swojej sieci
-IPAddress subnet(255, 255, 255, 0);
-
-#define ONE_WIRE_BUS D2
-OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature sensors(&oneWire);
-
-WiFiServer server(12345);
-float currentTemperature = -127.0;
+const char* webhook =
+"https://discordapp.com/api/webhooks/";
 
 void setup() {
+
   Serial.begin(115200);
-  sensors.begin();
 
-  // Konfiguracja statycznego IP
-  if (!WiFi.config(local_IP, gateway, subnet)) {
-    Serial.println("Błąd konfiguracji statycznego IP");
-  }
-
-  // Połączenie WiFi
   WiFi.begin(ssid, password);
-  Serial.print("Laczenie z WiFi");
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("Polaczono!");
-  Serial.print("IP: ");
-  Serial.println(WiFi.localIP());
 
-  // Uruchomienie serwera TCP
-  server.begin();
+  Serial.println("");
+  Serial.println("Połączono z WiFi");
 
-  // Usunięto mDNS
+  WiFiClientSecure client;
+  client.setInsecure();
+
+  HTTPClient https;
+
+  if (https.begin(client, webhook)) {
+
+    https.addHeader("Content-Type", "application/json");
+
+    String json =
+    "{\"content\":\"Wiadomość z ESP32!\"}";
+
+    int code = https.POST(json);
+
+    Serial.print("HTTP Code: ");
+    Serial.println(code);
+
+    String response = https.getString();
+
+    Serial.println(response);
+
+    https.end();
+  }
 }
 
 void loop() {
-  // Odświeżanie temperatury
-  sensors.requestTemperatures();
-  currentTemperature = sensors.getTempCByIndex(0);
 
-  // Obsługa klientów TCP
-  WiFiClient client = server.accept();
-  if (client) {
-    Serial.println("Klient się połączył");
-    String request = "";
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        if (c == '\n') break;
-        request += c;
-      }
-    }
-    Serial.print("Odebrane zapytanie: ");
-    Serial.println(request);
-
-    // Obsługa prostych zapytań HTTP
-    if (request.indexOf("GET / ") >= 0 || request.indexOf("GET / HTTP") >= 0) {
-      String response = "<html><head><title>Temperatura</title></head><body>";
-      response += "<h1>Aktualna temperatura: ";
-      response += String(currentTemperature, 2);
-      response += " &deg;C</h1></body></html>";
-      client.print("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n");
-      client.print(response);
-    } else {
-      // Zwrot samej wartości temperatury
-      String response = String(currentTemperature, 2) + " &deg;C\n";
-      client.print(response);
-    }
-    client.stop();
-    Serial.println("Klient rozłączony");
-  }
-
-  delay(100);
 }
